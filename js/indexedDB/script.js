ebb0dff87cc15333accbb9158ebad58f214267c4
Kembek <zevs.312.777@gmail.com>
2020-01-22 17:04:32 +0600

    init: indexedDB project was created

---
+++ js/indexedDB/script.js
@@ -0,0 +1,6 @@
+const text = document.querySelector("#text");
+
+window.onload = function() {
+  text.textContent =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, molestiae?";
+};
